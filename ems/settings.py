6961eaefef1a4e11bdb085a3f6da498a7f993e75
vivek <vivek.s2@accurogroup.com>
2023-02-23 15:22:03 -0500

    done vercel setting

--- ems/settings.py
+++ ems/settings.py
@@ -125,7 +125,7 @@ import os
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
 
     
 
